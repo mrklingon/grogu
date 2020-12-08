fdaf51c4db549128d30b3e54b8cb927977bf7421
Joel Peter Anderson <mrklingon@users.noreply.github.com>
2020-12-08 14:04:32 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -67,11 +67,16 @@ function talk () {
 }
 music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
 look_around()
+let listen = 0
 basic.forever(function () {
     if (130 < input.soundLevel()) {
-        talk()
-        soundExpression.giggle.play()
-    } else {
+        listen = 1
         look_around()
+    } else {
+        if (listen == 1) {
+            talk()
+            soundExpression.giggle.play()
+            listen = 0
+        }
     }
 })
